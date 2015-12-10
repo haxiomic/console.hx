b62724a9d8bdb52ad729b38928d92cc460f6f238
George Corney <haxiomic@gmail.com>
2015-12-10 21:50:05 +0000

    Various improvement

--- scriptutils/Print.hx
+++ scriptutils/Print.hx
@@ -5,23 +5,24 @@ import scriptutils.ConsoleColors;
 @:publicFields
 class Print{
 
-	static var verboseMode:Bool = #if debug true #else false #end;
+	static var verboseMode:Bool = false;
 	static var silent:Bool = false;
 	static var singleLineMode:Bool = false;
+	static var prefixNextLine:String = '';
 
 	static function fatal(s:String){
-		print('${BOLD}${BRIGHT_RED}Fatal:${RESET} $s');
+		printError('${BOLD}${BRIGHT_RED}Fatal:${RESET} $s');
 		if(singleLineMode)
 			endSingleLineMode();
 		Sys.exit(1);
 	}
 
 	static function error(s:String){
-		print('${BOLD}${RED}Error:${RESET} $s');
+		printError('${BOLD}${RED}Error:${RESET} $s');
 	}
 
 	static function warn(s:String){
-		print('${BOLD}${YELLOW}Warning:${RESET} $s');
+		printError('${BOLD}${YELLOW}Warning:${RESET} $s');
 	}
 
 	static function info(s:String){
@@ -44,26 +45,48 @@ class Print{
 	static function print(s:String){
 		if(silent) return;
 
-		if(singleLineMode){
-			//clear line
-			var stdout = Sys.stdout();
-			stdout.writeString('\033[1K');
-			stdout.flush();
-			Sys.print('\r');
+		s = prefixNextLine + s;
 
+		if(singleLineMode){
+			clearLine();
 			Sys.print(s);
 		}else{
 			Sys.println(s);
 		}
+
+		prefixNextLine = '';
+	}
+
+	static function printError(s:String){
+		if(silent) return;
+
+		s = prefixNextLine + s;
+
+		if(singleLineMode){
+			clearLine();
+			Sys.stderr().writeString(s);
+		}else{
+			Sys.stderr().writeString('$s\n');
+		}
+
+		prefixNextLine = '';
+	}
+
+	static function clearLine(){
+		//clear line
+		var stdout = Sys.stdout();
+		stdout.writeString('\033[1K');
+		stdout.flush();
+		Sys.print('\r');
 	}
 
 	static function beginSingleLineMode(){
 		singleLineMode = true;
 	}
 
-	static function endSingleLineMode(){
+	static function endSingleLineMode(newline:Bool = true){
 		singleLineMode = false;
-		print('');
+		if(newline) print('');
 	}
 	
 }
