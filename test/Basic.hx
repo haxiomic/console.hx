39fada6017a66aebd5164cb163754c080b28a96f
haxiomic <haxiomic@gmail.com>
2017-09-04 23:51:24 +0100

    Added a few more test strings

--- test/Basic.hx
+++ test/Basic.hx
@@ -8,7 +8,7 @@ class Basic {
 		Console.debug('Some debug information');
 
 		Console.printFormatted('<bold><underline>Bold and underlined</underline></bold> outsize');
-		Console.printFormatted('<red><b>Bold and red</b> some red text. <white>WHITE</white> continued red text</red>');
+		Console.printFormatted('<red><b>Bold and red</b> some red text. <light_white>WHITE</light_white> continued red text</red>');
 		Console.printFormatted('To format you can use HTML-like tags, for example \\<b>BOLD\\</b>');
 		Console.printFormatted('<light_green>What <b>about <b>Multiple</b> layers</b> of tags</light_green>');
 		Console.printFormatted('<bold><underline>Bold and underlined and <!>inverted!</!></bold></underline>');
@@ -23,6 +23,10 @@ class Basic {
 
 		Console.printFormatted('What about when you\'re trying to do math? a > b && a < b || > d');
 
+		Console.log('What <gray><b>about</> a</> short <red>hand</> for closing the last tag?');
+		Console.log('This could enable <#FF0000><b>Custom</#FF0000> colors</>');
+		Console.log('What about <{background:black; font-size:20px; color:white}>inline with <b>bold</b></> CSS? ');
+		Console.log('Can we </>close nothing?');
 	}
 
 }
