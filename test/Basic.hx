f931ffa6057bbd6265ca22ac37040a3a457f7281
haxiomic <haxiomic@gmail.com>
2017-09-05 02:30:54 +0100

    Added test lines for 256-color-palette

--- test/Basic.hx
+++ test/Basic.hx
@@ -1,10 +1,15 @@
 class Basic {
 
 	static function main() {
-		Console.log('Hello World');
-		Console.warn('Warning: ...');
-		Console.error('Error: something went wrong');
-		Console.success('Success!');
+		Console.printFormatted('<b><BLACK>black</> <RED>red</> <GREEN>green</> <YELLOW>yellow</> <BLUE>blue</> <MAGENTA>magenta</> <CYAN>cyan</> <WHITE>white</></>');
+		Console.printFormatted('<b><light_BLACK>black</> <light_RED>red</> <light_GREEN>green</> <light_YELLOW>yellow</> <light_BLUE>blue</> <light_MAGENTA>magenta</> <light_CYAN>cyan</> <light_WHITE>white</></>');
+		Console.printFormatted('<BLACK>███</><RED>███</><GREEN>███</><YELLOW>███</><BLUE>███</><MAGENTA>███</><CYAN>███</><WHITE>███</>');
+		Console.printFormatted('<light_BLACK>███</><light_RED>███</><light_GREEN>███</><light_YELLOW>███</><light_BLUE>███</><light_MAGENTA>███</><light_CYAN>███</><light_WHITE>███</>');
+
+		Console.log('<cyan>Hello</> <b>World!</>');
+		Console.warn('<u><yellow>Warning</yellow></>: ...');
+		Console.error('<red><b>Error:</></> something went wrong');
+		Console.success('<green>Success!</>');
 		Console.debug('Some debug information');
 
 		Console.printFormatted('<bold><underline>Bold and underlined</underline></bold> outsize');
@@ -25,8 +30,19 @@ class Basic {
 
 		Console.log('What <gray><b>about</> a</> short <red>hand</> for closing the last tag?');
 		Console.log('This could enable <#FF0000><b>Custom</#FF0000> colors</>');
-		Console.log('What about <{background:black; font-size:20px; color:white}>inline with <b>bold</b></> CSS? ');
+		Console.log('What about <bg_black><white><{background:black; font-size:20px; color:white}>inline with <b>bold</b></></white></bg_black> CSS? ');
 		Console.log('Can we </>close nothing?');
+
+		Console.warn('<yellow>Yellow</yellow>');
+		Console.warn('<bg_yellow><light_white>Awesome</light_white></bg_yellow>');
+
+		Console.log('<bg_light_white><black>BLACK</></> and <bg_black><light_white>WHITE</></>');
+
+		Console.log('What about more advanced <bg#FF0000><#00FF00>Backgrounds</></>?');
+
+		Console.log('And how do we deal with <#Invalid>custom colors</> like <#z>this?</>');
+
+		Console.log('<b><#17a9f5>Nice Blue</> and a <#333333>grayscale</></> <#313030>different gray</>');
 	}
 
 }
