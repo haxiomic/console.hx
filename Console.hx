084c01779692a2b522a33fa616fa1071c0ec3367
George Corney <haxiomic@gmail.com>
2021-04-20 19:11:03 +0100

    correct function name

--- Console.hx
+++ Console.hx
@@ -584,7 +584,7 @@ class Console {
 	}
 
 	#if macro
-	static function joinArgs(rest:Array<Expr>):ExprOf<String> {
+	static function joinArgExprs(rest:Array<Expr>):ExprOf<String> {
 		var msg:Expr = macro '';
 		var sepExpr: Expr = {expr: EConst(CString(argSeparator)), pos: haxe.macro.PositionTools.make({min: 0, max: 0, file: ""})};
 		for(i in 0...rest.length){
